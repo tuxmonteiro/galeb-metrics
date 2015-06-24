dd8bb32abacc191059e45dc890ebbaede2388a3c
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-06-24 09:53:34 -0300

    EventBus/QueueManager support removed

--- src/main/java/io/galeb/services/metrics/MetricsService.java
+++ src/main/java/io/galeb/services/metrics/MetricsService.java
@@ -16,22 +16,13 @@
 
 package io.galeb.services.metrics;
 
-import io.galeb.core.controller.EntityController.Action;
-import io.galeb.core.eventbus.Event;
-import io.galeb.core.json.JsonObject;
-import io.galeb.core.model.Backend;
-import io.galeb.core.model.BackendPool;
-import io.galeb.core.model.Entity;
 import io.galeb.core.model.Metrics;
-import io.galeb.core.model.Rule;
-import io.galeb.core.model.collections.BackendPoolCollection;
 import io.galeb.core.queue.QueueListener;
 import io.galeb.core.queue.QueueManager;
 import io.galeb.core.services.AbstractService;
 import io.galeb.core.statsd.StatsdClient;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -72,22 +63,22 @@ public class MetricsService extends AbstractService implements QueueListener {
     }
 
     private void startGateway() {
-        queueManager = eventbus.getQueueManager();
-        queueManager.register(this);
+//        queueManager = eventbus.getQueueManager();
+//        queueManager.register(this);
         final String host = System.getProperty(PREFIX + "." + STATSD_HOST);
         final Integer port = Integer.valueOf(System.getProperty(PREFIX + "." + STATSD_PORT));
         final String prefix = System.getProperty(PREFIX + "." + STATSD_PREFIX);
-        client.server(host).port(port).prefix(prefix);
+        client.host(host).port(port).prefix(prefix);
     }
 
     @Override
     public void onEvent(Serializable data) {
         if (data instanceof Metrics) {
             final Metrics metrics = (Metrics) data;
-            final Long requestTimeAvg = (Long) metrics.getProperty(Metrics.PROP_REQUESTTIME_AVG);
+            final Long requestTimeAvg = (Long) metrics.getProperty(StatsdClient.PROP_REQUESTTIME_AVG);
             sendRequestTime(metrics, requestTimeAvg);
             for (final String propertyName: metrics.getProperties().keySet()) {
-                if (propertyName.startsWith(Metrics.PROP_HTTPCODE_PREFIX)) {
+                if (propertyName.startsWith(StatsdClient.PROP_HTTPCODE_PREFIX)) {
                     final Integer statusCodeCount = (Integer) metrics.getProperty(propertyName);
                     sendStatusCodeCount(metrics, propertyName, statusCodeCount);
                 }
@@ -116,54 +107,54 @@ public class MetricsService extends AbstractService implements QueueListener {
 
     private void sendRequestTime(Metrics metrics, Long requestTimeAvg) {
         final String metricKeyPrefix = extractMetricKeyPrefix(metrics);
-        final String metricName = metricKeyPrefix + "." + Metrics.PROP_REQUESTTIME;
+        final String metricName = metricKeyPrefix + "." + StatsdClient.PROP_REQUESTTIME;
         client.timing(metricName, requestTimeAvg);
     }
 
-    @Override
-    public void onEvent(Event event) throws RuntimeException {
-        super.onEvent(event);
-
-        final JsonObject json = event.getData();
-        final Entity entity = (Entity) json.instanceOf(Entity.class);
-        final String backendEntityName = Backend.class.getSimpleName().toLowerCase();
-        Action action = Action.UNKNOWN;
-
-        if (event.getType() instanceof Action) {
-            action = (Action)event.getType();
-        }
-
-        if (entity.getEntityType().equals(backendEntityName) && action == Action.CHANGE) {
-
-            final BackendPoolCollection backendPoolsCollections = (BackendPoolCollection) farm.getCollection(BackendPool.class);
-            farm.getCollection(Backend.class).stream()
-                .filter(backend -> backend.getId().equals(entity.getId()))
-                .forEach(backend -> {
-
-                    final List<Entity> backendPools = backendPoolsCollections.getListByID(backend.getParentId());
-
-                    if (backendPools.isEmpty()) {
-                        logger.error(backend.getParentId() + " NOT FOUND");
-                        return;
-                    }
-
-                    final BackendPool backendPool = (BackendPool) backendPools.get(0);
-                    String virtualhostId = "";
-                    String lastVirtualhost = "";
-
-                    for (final Entity rule: farm.getCollection(Rule.class)){
-                        if (rule.getProperty(Rule.PROP_TARGET_ID).equals(backendPool.getId())) {
-                            virtualhostId = rule.getParentId();
-                            if (virtualhostId != null && !virtualhostId.equals(lastVirtualhost)) {
-                                lastVirtualhost = virtualhostId;
-                                final String metricKeyPrefix = extractMetricKeyPrefix(virtualhostId, backend.getId());
-                                final String metricName = metricKeyPrefix + "." + Backend.PROP_ACTIVECONN;
-                                client.gauge(metricName, Integer.valueOf(((Backend) backend).getConnections()).doubleValue());
-                            }
-                        }
-                    }
-                });
-        }
-    }
+//    @Override
+//    public void onEvent(Event event) throws RuntimeException {
+//        super.onEvent(event);
+//
+//        final JsonObject json = event.getData();
+//        final Entity entity = (Entity) json.instanceOf(Entity.class);
+//        final String backendEntityName = Backend.class.getSimpleName().toLowerCase();
+//        Action action = Action.UNKNOWN;
+//
+//        if (event.getType() instanceof Action) {
+//            action = (Action)event.getType();
+//        }
+//
+//        if (entity.getEntityType().equals(backendEntityName) && action == Action.CHANGE) {
+//
+//            final BackendPoolCollection backendPoolsCollections = (BackendPoolCollection) farm.getCollection(BackendPool.class);
+//            farm.getCollection(Backend.class).stream()
+//                .filter(backend -> backend.getId().equals(entity.getId()))
+//                .forEach(backend -> {
+//
+//                    final List<Entity> backendPools = backendPoolsCollections.getListByID(backend.getParentId());
+//
+//                    if (backendPools.isEmpty()) {
+//                        logger.error(backend.getParentId() + " NOT FOUND");
+//                        return;
+//                    }
+//
+//                    final BackendPool backendPool = (BackendPool) backendPools.get(0);
+//                    String virtualhostId = "";
+//                    String lastVirtualhost = "";
+//
+//                    for (final Entity rule: farm.getCollection(Rule.class)){
+//                        if (rule.getProperty(Rule.PROP_TARGET_ID).equals(backendPool.getId())) {
+//                            virtualhostId = rule.getParentId();
+//                            if (virtualhostId != null && !virtualhostId.equals(lastVirtualhost)) {
+//                                lastVirtualhost = virtualhostId;
+//                                final String metricKeyPrefix = extractMetricKeyPrefix(virtualhostId, backend.getId());
+//                                final String metricName = metricKeyPrefix + "." + Backend.PROP_ACTIVECONN;
+//                                client.gauge(metricName, Integer.valueOf(((Backend) backend).getConnections()).doubleValue());
+//                            }
+//                        }
+//                    }
+//                });
+//        }
+//    }
 
 }
